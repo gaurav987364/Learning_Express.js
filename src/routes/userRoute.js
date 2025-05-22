f67ae8d1dba54cdc8749c8de94f4d5d58b28633b
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-05-22 18:31:01 +0530

    Remove unnecessary blank line in userRoute.js for cleaner code

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,7 +3,6 @@ import { Router } from "express";
 const router = Router(); // router ko banaya;
 
 //making routes;
-
 // const createUser = (req, res) => {};
 // /api/users/ this end slash is below down wala;
 // router.route("/").post(createUser);
